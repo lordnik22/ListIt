ad4c7be0e0fb2ec706d0539a9c1cfac6e9482716
unknown <UttingeR@W722.berufzug.ch>
2016-06-20 15:18:41 +0200

    Signed-off-by: unknown <UttingeR@W722.berufzug.ch>

---
+++ resources/views/login.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.loggedOut')
+
+@section('title', 'Login')
+
+@section('content')
+<form method="POST" action="/login">
+    Benutzername:
+    <input type="text" name="user" value="{{ $name }}" />
+    Passwort:
+    <input type="password" name="password" />
+    <input type="submit"/>
+</form>
+
+@endsection
