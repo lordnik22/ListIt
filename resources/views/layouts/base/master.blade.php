328e8035acb73196b6a069a7c013ad1365298ff5
unknown <BlattleM@W720.berufzug.ch>
2016-06-21 16:27:52 +0200

    ersti commit vo mir

---
+++ resources/views/layouts/base/master.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="/css/style.css" />                     
+        <title>ListIt - @yield('title')</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
