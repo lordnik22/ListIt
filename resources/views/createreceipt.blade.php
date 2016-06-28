f78985ce39acb5333660861aa75daa563fe42934
lordnik22 <michi.b_98@hotmail.com>
2016-06-28 15:51:29 +0200

    css changes

--- resources/views/createreceipt.blade.php
+++ resources/views/createreceipt.blade.php
@@ -3,29 +3,35 @@
 @section('title', 'Quittung erstellen')
 
 @section('content')
-@parent
-<form method="POST" action="/createreceipt">
-    <label>
-        <span>Firma</span>
-        <input type="text" name="company" />
-    </label>
-    <span>Standort</span>
-    <label>
-        <span>Region</span>
-        <<input type="text" name="region" /> 
-    </label>
-    <label>
-        <span>Land</span>
-        <input type="text" name="country"/>
-    </label>    
-    <label>
-        <span>Strasse</span>
-        <input type="text" name="street" />
-    </label>
-    <label>
-        <span>StrassenNr</span>
-        <input type="text" name="streetNr" />
-    </label>      
-    <input type="submit" value="Speichern" />
-</form>
+<div id="addReceipt">
+    <form method="POST" action="/createreceipt">
+        <fieldset>
+            <legend>Quittung erstellen</legend>
+            <label>
+                <span>Firma</span>
+                <input type="text" name="company" />
+            </label>
+            <label>
+                <span>Standort</span>
+                <label>
+                    <span>Region</span>
+                    <input type="text" name="region" /> 
+                </label>
+                <label>
+                    <span>Land</span>
+                    <input type="text" name="country"/>
+                </label>    
+                <label>
+                    <span>Strasse</span>
+                    <input type="text" name="street" />
+                </label>
+                <label>
+                    <span>StrassenNr</span>
+                    <input type="text" name="streetNr" />
+                </label>      
+            </label>
+            <input type="submit" value="Speichern" />
+        </fieldset>
+    </form>
+</div>
 @endsection
